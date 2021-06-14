8a2eba35ffa742396d95862f0a221412b0e9a2c0
HanChean <chorhanchean97@gmail.com>
2021-06-14 15:25:16 +0800

    Version3 accepted

--- twoSums.py
+++ twoSums.py
@@ -3,26 +3,19 @@
 2) exactly 1 combination that adds up to target
 3) always have 1 combination
 """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-def main():
-    nums_int = list(map(int, input().strip(' []').split(',')))
-    #user will straight away input an integer list already, no need manually use loop to convert it
-    target : int = int(input())
-
-    #O(n^2) algorithm
-    hasFound : bool = False
-    for i in range(len(nums_int)):
-        for j in range(len(nums_int)):
-            if (j == i):
-                pass #skip
-            elif (nums_int[i] + nums_int[j] == target):
-                hasFound = True
-                print([i,j])
+        #O(n^2) algorithm
+        hasFound : bool = False
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if (j == i):
+                    pass #skip
+                elif (nums[i] + nums[j] == target):
+                    hasFound = True
+                    return [i,j]
+                    break
+            if hasFound:
                 break
-        if hasFound:
-            break
-
-
-if __name__ == "__main__":
-    main()
 
